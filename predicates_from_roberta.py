91ac78711486fcc2bbdde5a7dff0b1465e7bdb08
Christian Clark <clark.3664@osu.edu>
2023-08-05 10:05:14 -0400

    script to extract common predicates from RoBERTa based on templates

---
+++ predicates_from_roberta.py
@@ -0,0 +1,86 @@
+import argparse, torch
+from nltk.stem import WordNetLemmatizer
+from transformers import AutoTokenizer, RobertaForMaskedLM
+
+
+MASK_ID = 50264
+
+TEMPLATES = {
+    "adj": "One of them is very <mask>.",
+    "noun": "The child saw the <mask>.",
+    "vintrans": "One of them will <mask>.",
+    "vtrans": "One of them will <mask> another one."
+}
+    #"noun": "This is the <mask>.",
+    #"noun": "Here is a <mask>.",
+    #"noun": "I saw the <mask>.",
+
+argparser = argparse.ArgumentParser(
+    """
+    Extract likely predicates from RoBERTa.
+    """
+)
+
+argparser.add_argument(
+    "pos", choices=["adj", "noun", "vintrans", "vtrans"],
+    help="part of speech"
+)
+
+argparser.add_argument(
+    "-n", type=int, default=50,
+    help="number of predicates"
+)
+
+argparser.add_argument(
+    "-a", "--alphabetize", default=False, action="store_true",
+    help="alphabetize results"
+)
+
+# TODO option for model?
+#    argparser.add_argument(
+#        "-m",
+#        "--model",
+#        default="125m"
+#    )
+
+args = argparser.parse_args()
+pos = args.pos
+
+# location of models: /home/clark.3664/.cache/huggingface/hub/models--roberta-base/snapshots/bc2764f8af2e92b6eb5679868df33e224075ca68
+tokenizer = AutoTokenizer.from_pretrained("roberta-base")
+model = RobertaForMaskedLM.from_pretrained("roberta-base")
+lemmatizer = WordNetLemmatizer()
+template = TEMPLATES[pos]
+
+model_input = tokenizer(template, return_tensors="pt")
+input_ids = model_input.input_ids[0]
+mask_index = input_ids.tolist().index(MASK_ID)
+logits = model(**model_input).logits
+mask_logits = logits[0, mask_index]
+sorted_vals, sorted_indices = torch.sort(mask_logits, descending=True)
+
+words = list()
+for ix in sorted_indices:
+    word = tokenizer.decode(ix).strip()
+    if not word.isalpha(): continue
+    if pos == "adj":
+        word = lemmatizer.lemmatize(word.lower(), pos='a')
+        if word not in words:
+            words.append(word)
+    elif pos == "noun":
+        word = lemmatizer.lemmatize(word.lower(), pos='n')
+        if word not in words:
+            words.append(word)
+    else:
+        assert pos == "vintrans" or pos == "vtrans"
+        word = lemmatizer.lemmatize(word.lower(), pos='v')
+        if word not in words:
+            words.append(word)
+    if len(words) == args.n: break
+
+if args.alphabetize:
+    words = sorted(words)
+
+for word in words:
+    print(word)
+
